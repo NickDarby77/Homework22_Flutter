6ad53dfb270574c43e8a738789b6afbf8c12690c
NickDarby77 <132682600+NickDarby77@users.noreply.github.com>
2023-09-09 04:12:39 +0600

    NewCommit

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -4,7 +4,6 @@ abstract class AppColors {
   static Color backgroundColor = const Color(0xffF9F9F9);
   static Color boxDecorationbgColor = const Color.fromARGB(255, 248, 248, 219);
   static Color green = const Color(0xff5EE0B1);
-  static Color borderColor =  Colors.green;
-  static Color borderColor2 =  Colors.transparent;
-
+  static Color borderColor = Colors.green;
+  static Color borderColor2 = Colors.transparent;
 }
